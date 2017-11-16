48698d91abc5672daa4751f5721e72c0550342a2
sebastianWV <stickert@worldviz.com>
2017-11-16 23:49:06 +0100

    added unique key on name for preset table revision

--- alembic/versions/003_7fe022eacc36_added_preset_table.py
+++ alembic/versions/003_7fe022eacc36_added_preset_table.py
@@ -1,8 +1,8 @@
 """added preset table
 
-Revision ID: 003_257b4479748d
+Revision ID: 003_7fe022eacc36
 Revises: 002_88971660dfff
-Create Date: 2017-11-16 23:29:14.673000
+Create Date: 2017-11-16 23:47:20.895000
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as db
 
 
 # revision identifiers, used by Alembic.
-revision = '003_257b4479748d'
+revision = '003_7fe022eacc36'
 down_revision = '002_88971660dfff'
 branch_labels = None
 depends_on = None
@@ -21,8 +21,10 @@ def upgrade():
       db.Column('id', db.Integer(), nullable=False),
       db.Column('name', db.String(length=256), nullable=False),
       db.Column('json', db.String(length=4096), nullable=False),
-      db.PrimaryKeyConstraint('id')
+      db.PrimaryKeyConstraint('id'),
+      db.UniqueConstraint('name')
     )
+
 
 def downgrade():
     op.drop_table('preset')
